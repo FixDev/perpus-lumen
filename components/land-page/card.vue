bc2bbeeebe2e4d08edf65b6a9817db55818499b6
MuchlasS <muchlas148@gmail.com>
2020-03-20 11:01:14 +0700

    mencoba 'get api' tetapi menampilkan galat

---
+++ components/land-page/card.vue
@@ -0,0 +1,86 @@
+<template>
+    <div>
+        <v-container fluid>
+            <v-row>
+                <v-card
+                    class="mx-auto"
+                    max-width="300px"
+                >
+                    <v-img
+                    class="white--text align-end"
+                    height="200px"
+                    src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+                    >
+                    <v-card-title>{{ judul }}</v-card-title>
+                    </v-img>
+
+                    <v-card-subtitle class="pb-0">{{ penulis }}</v-card-subtitle>
+
+                    <v-card-text class="text--primary">
+                    <div>{{ deskripsi }}</div>
+                    </v-card-text>
+
+                    <v-card-actions>
+                    <v-btn
+                        color="info"
+                    >
+                        Details
+                    </v-btn>
+
+                    <v-btn
+                        color="orange"
+                        text
+                    >
+                        Lainnya
+                    </v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-row>
+        </v-container>
+    </div>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      show: false,
+    }),
+
+    props:{
+        id:{
+            type: String,
+            default: ''
+        },
+
+        judul:{
+            type: String,
+            default: ''
+        },
+
+        deskripsi:{
+            type: String,
+            default: ''
+        },
+
+        penulis:{
+            type: String,
+            default: ''
+        },
+
+        gambar:{
+            type: String,
+            default: ''
+        },
+
+        created_at:{
+            type: Date,
+            default: ''
+        },
+
+        updated_at:{
+            type: Date,
+            default: ''
+        }
+    }
+  }
+</script>
---
+++ components/land-page/list-kat.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+      <v-row align="center" justify="center">
+        <v-btn-toggle
+          v-model="text"
+          tile
+          color="info darken-2"
+          group
+        >
+          <v-btn value="left">
+            Left
+          </v-btn>
+
+          <v-btn value="center">
+            Center
+          </v-btn>
+
+          <v-btn value="right">
+            Right
+          </v-btn>
+
+          <v-btn value="justify">
+            Justify
+          </v-btn>
+        </v-btn-toggle>
+      </v-row>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        text: 'center',
+        icon: 'justify',
+        toggle_none: null,
+        toggle_one: 0,
+        toggle_exclusive: 2,
+        toggle_multiple: [0, 1, 2],
+      }
+    },
+  }
+</script>
+
+<style>
+
+</style>
