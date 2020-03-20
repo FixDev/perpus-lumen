44eab79ec31e584b82873abe89288b7f521cf5b4
Muhammad Fikri <fikrimuhammad2016@gmail.com>
2020-03-20 11:57:12 +0700

    fixed api crops

---
+++ pages/buku/index.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-card>
+    <v-card-title>
+      <v-text-field
+        v-model="search"
+        label="Search"
+        single-line
+        hide-details
+      ></v-text-field>
+    </v-card-title>
+    <v-data-table
+      :headers="headers"
+      :items="items"
+      :search="search"
+    ></v-data-table>
+  </v-card>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      search: "",
+      headers: [
+        {
+          text: "Judul", value: "judul"
+        },
+        { text: "Deskripsi", value: "deskripsi" },
+        { text: "Penulis", value: "penulis" },
+        { text: "Kategori", value: "kategori_id" }
+      ],
+      items: []
+    };
+  },
+  mounted() {
+    this.getData();
+  },
+  methods: {
+    async getData() {
+      const res = await this.$axios.get("http://192.168.1.20:8000/api/buku");
+      this.items = res.data;
+    }
+  }
+};
+</script>
+
+<style></style>
